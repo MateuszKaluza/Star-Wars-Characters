02b6d675e59cbb57af002db02c0d054b1a3dcf50
Mateusz Kaluza <mateo.kaluza@gmail.com>
2019-12-16 23:54:45 +0100

    Fix debounce

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, {useState} from 'react';
 import SearchInput from "./common/SearchInput";
 import Container from "@material-ui/core/Container";
 import Characters from "./Characters/Characters";
+import debounce from "lodash.debounce";
 
 function App() {
     const [queryString, setQueryString] = useState('');
+    const debouncedHandler = debounce(setQueryString, 300);
 
     return (
         <div>
             <Container maxWidth="sm">
-                <SearchInput handleChange={setQueryString}/>
+                <SearchInput handleChange={debouncedHandler}/>
                 <Characters queryString={queryString}/>
             </Container>
         </div>
--- src/common/SearchInput.jsx
+++ src/common/SearchInput.jsx
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import PropTypes from 'prop-types';
-import debounce from 'lodash.debounce';
 
 function SearchInput(props) {
-    const [name, setName] = useState('');
-    const debouncedHandler = debounce(props.handleChange, 300);
 
     const handleChange = event => {
         const value = event.target.value;
-        setName(value);
-        debouncedHandler(value);
+         props.handleChange(value);
     };
 
     return (
         <TextField
             label="Name"
             fullWidth
-            value={name}
             onChange={handleChange}
             margin="normal"
             autoFocus
